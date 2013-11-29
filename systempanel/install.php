f11c1bd3da5f0e03d7abf0752c22c51648cfcb79
Matteo Bruni <ares@MacBook-di-Caelan-4.local>
2013-11-29 01:12:11 +0100

    Fix install

--- systempanel/install.php
+++ systempanel/install.php
@@ -13,6 +13,8 @@
 		
 		file_put_contents('../framework/db.conf.inc.php', $data);
 		
+		exec("/usr/bin/sudo chmod 775 ../framework/db.conf.inc.php");
+		
 		require_once('../framework/db.conf.inc.php');
 		
 		$mysqli = new mysqli($db_host, $db_user, $db_pass, $db_name);
@@ -26,15 +28,25 @@
 		  `name` varchar(10) NOT NULL,
 		  `description` varchar(250) NOT NULL,
 		  PRIMARY KEY (`name`)
-		) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+		)";
 		
-		INSERT INTO `hash` (`name`, `description`) VALUES
+		$stmt = $mysqli->stmt_init();
+		$stmt->prepare($query); 
+		$stmt->execute();
+		$stmt->close();
+		
+		$query = "INSERT INTO `hash` (`name`, `description`) VALUES
 		('md5', 'MD5'),
 		('sha1', 'SHA 1'),
 		('sha256', 'SHA 256'),
-		('sha512', 'SHA 512');
+		('sha512', 'SHA 512')";
 		
-		CREATE TABLE `session` (
+		$stmt = $mysqli->stmt_init();
+		$stmt->prepare($query); 
+		$stmt->execute();
+		$stmt->close();
+		
+		$query = "CREATE TABLE `session` (
 		  `id` int(11) NOT NULL AUTO_INCREMENT,
 		  `id_user` int(11) NOT NULL,
 		  `sid` varchar(100) NULL,
@@ -42,9 +54,14 @@
 		  `device` varchar(100) NOT NULL,
 		  PRIMARY KEY (`id`),
 		  KEY `id_user` (`id_user`)
-		) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=24 ;
+		)";
 		
-		CREATE TABLE `user` (
+		$stmt = $mysqli->stmt_init();
+		$stmt->prepare($query);
+		$stmt->execute();
+		$stmt->close();
+		
+		$query = "CREATE TABLE `user` (
 		  `id` int(11) NOT NULL AUTO_INCREMENT,
 		  `id_hash` varchar(10) NOT NULL,
 		  `username` varchar(100) NOT NULL,
@@ -53,12 +70,23 @@
 		  PRIMARY KEY (`id`),
 		  UNIQUE KEY `username` (`username`),
 		  KEY `id_hash` (`id_hash`)
-		) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+		)";
 		
-		INSERT INTO `user` (`id`, `id_hash`, `username`, `password`, `admin`) VALUES
-		(1, 'sha512', ?, ?, b'1');
+		$stmt = $mysqli->stmt_init();
+		$stmt->prepare($query); 
+		$stmt->execute();
+		$stmt->close();
 		
-		CREATE TABLE `user_widget` (
+		$query = "INSERT INTO `user` (`id`, `id_hash`, `username`, `password`, `admin`) VALUES
+		(1, 'sha512', ?, ?, b'1')";
+		
+		$stmt = $mysqli->stmt_init();
+		$stmt->prepare($query);
+		$stmt->bind_param("ss", $_POST['admin_username'], hash('sha512', $_POST['admin_password'])); 
+		$stmt->execute();
+		$stmt->close();
+		
+		$query = "CREATE TABLE `user_widget` (
 		  `id` int(11) NOT NULL AUTO_INCREMENT,
 		  `id_user` int(11) NOT NULL,
 		  `id_widget` int(11) NOT NULL,
@@ -69,27 +97,31 @@
 		  PRIMARY KEY (`id`),
 		  KEY `id_user` (`id_user`),
 		  KEY `id_widget` (`id_widget`)
-		) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=14 ;
+		)";
 		
-		DECLARE @id_user int(11);
-		SET @id_user = (SELECT id FROM `user`)
+		$stmt = $mysqli->stmt_init();
+		$stmt->prepare($query); 
+		$stmt->execute();
+		$stmt->close();
 		
-		INSERT INTO `user_widget` (`id`, `id_user`, `id_widget`, `id_html`, `position`, `enabled`, `visible`) VALUES
+		$query = "INSERT INTO `user_widget` (`id`, `id_user`, `id_widget`, `id_html`, `position`, `enabled`, `visible`) VALUES
 		(1, 1, 1, 'sys_info', 0, b'1', b'1'),
 		(2, 1, 2, 'cpu_info', 1, b'1', b'1'),
-		(3, 1, 3, 'camera', 2, b'1', b'1'),
-		(4, 1, 4, 'usb_info', 3, b'1', b'0'),
-		(5, 1, 5, 'network', 4, b'1', b'0'),
-		(6, 1, 6, 'memory_info', 5, b'1', b'1'),
-		(7, 1, 7, 'updates', 8, b'1', b'0'),
-		(8, 1, 8, 'disks', 9, b'1', b'0'),
-		(9, 1, 9, 'processes', 10, b'1', b'0'),
-		(12, 1, 10, 'power', 7, b'1', b'1'),
-		(13, 1, 11, 'transmission', 6, b'1', b'1'),
-		(14, 1, 12, 'cpu_graph', 6, b'1', b'1'),
-		(15, 1, 13, 'temp_graph', 6, b'1', b'1');
+		(3, 1, 4, 'usb_info', 2, b'1', b'0'),
+		(4, 1, 5, 'network', 3, b'1', b'0'),
+		(5, 1, 6, 'memory_info', 4, b'1', b'1'),
+		(6, 1, 7, 'updates', 8, b'1', b'0'),
+		(7, 1, 8, 'disks', 9, b'1', b'0'),
+		(8, 1, 9, 'processes', 10, b'1', b'0'),
+		(9, 1, 12, 'cpu_graph', 5, b'1', b'1'),
+		(10, 1, 13, 'temp_graph', 6, b'1', b'1')";
 		
-		CREATE TABLE `widget` (
+		$stmt = $mysqli->stmt_init();
+		$stmt->prepare($query); 
+		$stmt->execute();
+		$stmt->close();
+		
+		$query = "CREATE TABLE `widget` (
 		  `id` int(11) NOT NULL AUTO_INCREMENT,
 		  `columns` int(11) NOT NULL DEFAULT '6',
 		  `updatetime` int(11) NOT NULL DEFAULT '1000',
@@ -98,9 +130,14 @@
 		  `templatefile` varchar(250) NOT NULL,
 		  `requireadmin` bit(1) NOT NULL DEFAULT b'0',
 		  PRIMARY KEY (`id`)
-		) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=12 ;
+		)";
 		
-		INSERT INTO `widget` (`id`, `columns`, `updatetime`, `title`, `phpfile`, `templatefile`, `requireadmin`) VALUES
+		$stmt = $mysqli->stmt_init();
+		$stmt->prepare($query); 
+		$stmt->execute();
+		$stmt->close();
+		
+		$query = "INSERT INTO `widget` (`id`, `columns`, `updatetime`, `title`, `phpfile`, `templatefile`, `requireadmin`) VALUES
 		(1, 4, 1000, 'System Info', 'system', 'general_info.tpl', b'0'),
 		(2, 4, 1000, 'CPU Info', 'processor', 'cpu_info.tpl', b'0'),
 		(3, 4, 300000, 'Camera Image', 'raspistill', 'raspistill.tpl', b'1'),
@@ -113,19 +150,10 @@
 		(10, 2, 0, 'Power', 'power', 'power.tpl', b'1'),
 		(11, 4, 1000, 'Transmission', 'transmission', 'transmission.tpl', b'0'),
 		(12, 4, 1000, 'CPU Load', 'cpu_graph', 'cpu_graph.tpl', b'0'),
-		(13, 4, 1000, 'Temperature', 'temp_graph', 'temp_graph.tpl', b'0');
-		
-		ALTER TABLE `user`
-		  ADD CONSTRAINT `user_ibfk_1` FOREIGN KEY (`id_hash`) REFERENCES `hash` (`name`) ON UPDATE CASCADE;
-		
-		ALTER TABLE `user_widget`
-		  ADD CONSTRAINT `user_widget_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-		  ADD CONSTRAINT `user_widget_ibfk_3` FOREIGN KEY (`id_widget`) REFERENCES `widget` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
-		";
+		(13, 2, 1000, 'Temperature', 'temp_graph', 'temp_graph.tpl', b'0')";
 		
 		$stmt = $mysqli->stmt_init();
-		$stmt->prepare($query);
-		$stmt->bind_param("ss", $_POST['admin_username'], hash('sha512', $_POST['admin_password'])); 
+		$stmt->prepare($query); 
 		$stmt->execute();
 		$stmt->close();
 		
--- systempanel/templates/install.tpl
+++ systempanel/templates/install.tpl
@@ -5,6 +5,7 @@
 </head>
 <body>
 	<div class="container">
+		<form method="post" action="install.php">
 		<div class="row">
 			<div class="col-xs-12">
 				<div class="panel panel-success">
@@ -12,25 +13,22 @@
 						<h3>Database</h3>
 					</div>
 					<div class="panel-body">
-						<form method="post" action="install.php">
-							<div class="form-group">
-								<label for="db_host">Host</label>
-								<input type="text" class="form-control" id="db_host" name="db_host" placeholder="Host">
-							</div>
-							<div class="form-group">
-								<label for="db_username">Username</label>
-								<input type="text" class="form-control" id="db_username" name="db_username" placeholder="Username">
-							</div>
-							<div class="form-group">
-								<label for="db_password">Password</label>
-								<input type="password" class="form-control" id="db_password" name="db_password" placeholder="Password">
-							</div>
-							<div class="form-group">
-								<label for="db_name">Name</label>
-								<input type="text" class="form-control" id="db_name" name="db_name" placeholder="Password">
-							</div>
-							<button type="submit" name="login" class="btn btn-primary">Submit</button>
-						</form>
+						<div class="form-group">
+							<label for="db_host">Host</label>
+							<input type="text" class="form-control" id="db_host" name="db_host" placeholder="Host">
+						</div>
+						<div class="form-group">
+							<label for="db_username">Username</label>
+							<input type="text" class="form-control" id="db_username" name="db_username" placeholder="Username">
+						</div>
+						<div class="form-group">
+							<label for="db_password">Password</label>
+							<input type="password" class="form-control" id="db_password" name="db_password" placeholder="Password">
+						</div>
+						<div class="form-group">
+							<label for="db_name">Name</label>
+							<input type="text" class="form-control" id="db_name" name="db_name" placeholder="Password">
+						</div>
 					</div>
 				</div>
 			</div>
@@ -42,21 +40,20 @@
 						<h3>Admin</h3>
 					</div>
 					<div class="panel-body">
-						<form method="post" action="install.php">
-							<div class="form-group">
-								<label for="admin_username">Username</label>
-								<input type="text" class="form-control" id="admin_username" name="admin_username" placeholder="Username">
-							</div>
-							<div class="form-group">
-								<label for="admin_password">Password</label>
-								<input type="password" class="form-control" id="admin_password" name="admin_password" placeholder="Password">
-							</div>
-							<button type="submit" name="login" class="btn btn-primary">Submit</button>
-						</form>
+						<div class="form-group">
+							<label for="admin_username">Username</label>
+							<input type="text" class="form-control" id="admin_username" name="admin_username" placeholder="Username">
+						</div>
+						<div class="form-group">
+							<label for="admin_password">Password</label>
+							<input type="password" class="form-control" id="admin_password" name="admin_password" placeholder="Password">
+						</div>
+						<button type="submit" name="login" class="btn btn-primary">Submit</button>
 					</div>
 				</div>
 			</div>
 		</div>
+		</form>
 	</div>
 </body>
 </html>
