0107c5b1cce72d1bf415af7fb0684d6f0b636dcc
Matteo Bruni <ares@MacBook-di-Caelan-4.local>
2013-11-29 01:16:17 +0100

    Fix index

--- systempanel/index.php
+++ systempanel/index.php
@@ -3,28 +3,30 @@
 	{
 		header('Location: install.php');
 	}
+	else
+	{
+		session_start();
 
-	session_start();
-
-	if (!isset($_GET['sid']))
-		header('Location: login.php');
-	
-	require_once('../framework/settings.inc.php');
-	
-	$sid = $_GET['sid'];
-	$session_id = session_id($sid);
-	
-	if (empty($session_id) || Settings::get_user_info($sid) == null) header('Location: login.php');
-	
-	require_once('Smarty.class.php');
-	require_once('../framework/widget.inc.php');
-	
-	$settings = new Settings($sid);
-	
-	$smarty = new Smarty();
-	
-	$smarty->assign('widgets', $settings->widgets);
-	$smarty->assign('sid', $sid);
-	
-	$smarty->display('index.tpl');
+		if (!isset($_GET['sid']))
+			header('Location: login.php');
+		
+		require_once('../framework/settings.inc.php');
+		
+		$sid = $_GET['sid'];
+		$session_id = session_id($sid);
+		
+		if (empty($session_id) || Settings::get_user_info($sid) == null) header('Location: login.php');
+		
+		require_once('Smarty.class.php');
+		require_once('../framework/widget.inc.php');
+		
+		$settings = new Settings($sid);
+		
+		$smarty = new Smarty();
+		
+		$smarty->assign('widgets', $settings->widgets);
+		$smarty->assign('sid', $sid);
+		
+		$smarty->display('index.tpl');	
+	}
 ?>
