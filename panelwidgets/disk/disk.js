75d1bc6fa11b56104d78a14d65fccd0abc962cf7
Matteo Bruni <ar3shack@gmail.com>
2014-01-06 13:04:07 +0100

    Disk widget NodeJS ready

---
+++ panelwidgets/disk/disk.js
@@ -0,0 +1,33 @@
+exports.data = function (cb) {
+	var util = require('util');
+	var exec = require('child_process').exec;
+	var res = { disks: [] };
+	
+	exec("df -h -T -x tmpfs -x devtmpfs -x rootfs", function (err, stdout, stderr) {
+		var diskfree = stdout.split(/[\r\n]{1,2}/);
+		
+		delete diskfree[0];
+		
+		for (var c = 0; c < diskfree.length; c++)
+		{
+			if (diskfree[c]) {
+				var splitArr = diskfree[c].split(/\s+/);
+				var obj = {
+					typex: splitArr[1],
+					size: splitArr[2],
+					used: splitArr[3],
+					avail: splitArr[4],
+					percent_used: parseInt(splitArr[5].replace('%', ''))
+				};
+				obj.percent_avail = 100 - obj.percent_used;
+				res.disks.push(obj);
+			}
+		}
+		
+		cb(res);
+	});
+};
+
+exports.manage_post = function (post, cb) {
+	cb(0);
+};
