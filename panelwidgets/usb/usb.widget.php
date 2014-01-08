37786c18a2b471b7b539aaf0d990786906018731
Matteo Bruni <ares@MacBook-Air-di-Matteo.local>
2014-01-08 20:59:31 +0100

    USB Widget NodeJS-ready

--- panelwidgets/usb/usb.widget.php
+++ panelwidgets/usb/usb.widget.php
@@ -10,12 +10,6 @@
 		
 		public function __construct($usbinfo)
 		{
-			/*
-Bus 001 Device 002: ID 0424:9512 Standard Microsystems Corp. 
-Bus 001 Device 001: ID 1d6b:0002 Linux Foundation 2.0 root hub
-Bus 001 Device 003: ID 0424:ec00 Standard Microsystems Corp.
-			 */
-			
 			list($busString ,$bus, $deviceString, $device, $idString, $id, $name) = preg_split("/\s+/", $usbinfo, 7);
 			
 			$this->bus = $bus;
