79709c5fee8cd1b018eab252e6ddc92a60b74fed
Matteo Bruni <ares@MacBook-Air-di-Matteo.local>
2014-01-14 00:44:59 +0100

    Prepared widget_server.js for sockets.io, tested with sample client. Will be implemented ASAP

--- panelwidgets/widget_server.js
+++ panelwidgets/widget_server.js
@@ -1,4 +1,5 @@
 var http = require('http');
+var socket_io = require('socket.io');
 var nodeStatic = require('node-static');
 var file = new nodeStatic.Server();
 var url = require('url');
@@ -54,64 +55,83 @@ function createUserWidgetFromSQLRow(row, cb) {
 	cb(user_widget);
 }
 
+function getUserWidget(json, sid, widget_id, post_params, cb) {
+	var mysqlJSON = require('./db_conn.json');
+	var connection = mysql.createConnection(mysqlJSON);
+	
+	connection.query('CALL GetUserInfo(?)', [sid], function (err, rows) {
+		if (!err) {
+			if (rows[0].length) {
+				connection.query('CALL LoadSettings(?)', [sid], function (err, rows) {
+					if (!err) {
+						var user_widget = null;
+						
+						for (var c = 0; c < rows[0].length && !user_widget; c++) {
+							if (rows[0][c].uwid == widget_id) {
+								createUserWidgetFromSQLRow(rows[0][c], function (uw) {
+									user_widget = uw;
+								});
+							}
+						}
+						
+						if (user_widget) {
+							var folder = './' + user_widget.widget.folder;
+							var path = folder + '/' + user_widget.widget.phpfile + '.js';
+							var loaded_widget = require(path);
+							loaded_widget.manage_post(post_params, function (result, output) {
+								if (result) {
+									cb(200, 'text/plain', output);
+								} else {
+									loaded_widget.data(function (widget_data) {
+										if (json) {
+											cb(200, 'application/json', widget_data);									
+										} else {
+											Bliss = require('bliss');
+											bliss = new Bliss();
+											template = bliss.compileFile(folder + '/' + user_widget.widget.templatefile.replace('.tpl', ''));
+											output = template(widget_data, user_widget, sid);
+											cb(200, 'text/html', output);
+										}
+									});
+								}
+							});
+						} else {
+							cb(500, 'text/plain', '');
+						}
+					} else {
+						console.log(err);
+					}
+				});
+			}
+		} else {
+			console.log(err);
+		}
+	});
+}
+
 var server = http.createServer(function (req, res) {
 	var pre = {};
 	initPredefinedVariables(req, res, pre, function () {
-		var json = pre.get.json || false;
-		var sid = pre.get.sid;
-		var mysqlJSON = require('./db_conn.json');
-		var connection = mysql.createConnection(mysqlJSON);
+		var json = pre.post.json || false;
+		var sid = pre.post.sid;
+		var widget_id = pre.post["widget-id"];
 		
-		connection.query('CALL GetUserInfo(?)', [sid], function (err, rows) {
-			if (!err) {
-				if (rows[0].length) {
-					connection.query('CALL LoadSettings(?)', [sid], function (err, rows) {
-						if (!err) {
-							var user_widget = null;
-							
-							for (var c = 0; c < rows[0].length && !user_widget; c++) {
-								if (rows[0][c].uwid == pre.get["widget-id"]) {
-									createUserWidgetFromSQLRow(rows[0][c], function (uw) {
-										user_widget = uw;
-									});
-								}
-							}
-							
-							if (user_widget) {
-								var folder = './' + user_widget.widget.folder;
-								var path = folder + '/' + user_widget.widget.phpfile + '.js';
-								var loaded_widget = require(path);
-								loaded_widget.manage_post(pre.post, function (result, output) {
-									if (result) {
-										res.writeHead(200, { 'Content-Type': 'text/plain' });
-										res.end(output);
-									} else {
-										loaded_widget.data(function (widget_data) {
-											if (json) {
-												res.writeHead(200, { 'Content-Type': 'application/json' });
-												res.end(JSON.stringify(widget_data));										
-											} else {
-												Bliss = require('bliss');
-												bliss = new Bliss();
-												template = bliss.compileFile(folder + '/' + user_widget.widget.templatefile.replace('.tpl', ''));
-												output = template(widget_data, user_widget, sid);
-												res.writeHead(500, { 'Content-Type': 'text/html' });
-												res.end(output);
-											}
-										});
-									}
-								});
-							} else {
-								res.writeHead(500, { 'Content-Type': 'text/plain' });
-								res.end();
-							}
-						}
-					});
-				}
-			}
+		getUserWidget(json, sid, widget_id, pre_post, function (statusCode, contentType, output) {
+			res.writeHead(statusCode, { 'Content-Type': contentType });
+			res.end(output);
 		});
 	});
 });
 
-server.listen(1337, function () {
+server.listen(1337);
+
+var io = socket_io.listen(server);
+
+io.sockets.on('connection', function (socket) {
+  socket.on('requestdata', function (data) {
+  	getUserWidget(data.json, data.sid, data["widget-id"], {}, function (statusCode, contentType, output) {
+  		socket.emit('newdata', { output: output, statusCode: statusCode, contentType: contentType });
+	});
+    
+  });
 });
