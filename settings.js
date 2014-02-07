7c58e2ac08610f3e7b136b1a049e54bfbd0e9472
Matteo Bruni <ar3shack@gmail.com>
2014-02-07 01:02:25 +0100

    Managed post/forms of settings page. All settings need to be retested. Next step is changing view engine because of problems with views.

--- settings.js
+++ settings.js
@@ -1,13 +1,52 @@
 exports.page = function(req, res, app, next) {
-	if (req.body && req.body.sid) {
-		//TODO: Manage settings post
-	} else {
-		//TODO: Manage settings get
-		var sid = req.params.sid;
-		if (sid) {
-			//TODO: Verificare la validità della sessione
-			var settings = require('./framework/settings.js');
+	var settings = require('./framework/settings.js');
+	var sid = req.params.sid;
+	if (sid) {
+		if (req.body && req.body != {}) {
+			if (req.body.visibility !== undefined) {
+				var widget_id = req.body["widget-id"];
 
+				settings.toggleWidgetVisibility(sid, widget_id, req.body.visibility);
+
+				return res.send({ visible: req.body.visibility });
+			} else if (req.body.enable !== undefined) {
+				var widget_id = req.body["widget-id"];
+
+				settings.toggleWidgetState(sid, widget_id, req.body.enable);
+
+				return res.send({ enabled: req.body.enable });
+			} else if (req.body.save_user) {
+				var username = req.body.username;
+				var hashmethod = req.body.hashmethod;
+				var crypto = require('crypto');
+				var password = crypto.createHash(hashmethod).update(req.body.password).digest('hex');
+
+				settings.save_user(sid, username, password, hashmethod);
+				return res.send();
+			} else if (req.body.assign_widget) {
+				var widget = {
+					id_html: req.body.widget_id_html,
+					poistion: req.body.widget_position
+				};
+				var wid = req.body.wid;
+
+				settings.create_user_widget(sid, widget, wid);
+
+				return res.send();
+			} else if (req.body.widget_action) {
+				var widget = {
+					id: req.body.widget_id,
+					id_html: req.body.widget_id_html,
+					poistion: req.body.widget_position
+				};
+
+				if (req.body.widget_action == 'save') {
+					settings.save_user_widget(sid, widget);
+				}else if (req.body.widget_action == 'delete') {
+					settings.delete_user_widget(sid, widget);
+				}
+			}
+		} else {
 			settings.load(sid, function(user) {
 				settings.get_hash_methods(sid, function (hashes) {
 					var current_url = req.headers.host.split(':')[0];
@@ -22,8 +61,8 @@ exports.page = function(req, res, app, next) {
 					});
 				});
 			});
-		} else {
-			return next();
-		}
+
+		}		} else {
+		return next();
 	}
 };
